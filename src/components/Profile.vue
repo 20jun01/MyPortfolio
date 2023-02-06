66add07c440860f82068a4eb965aa0c10f71f161
20jun01 <10junjun20@gmail.com>
2023-02-06 16:38:38 +0900

    ⚡️ change to v-row and col

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,26 +1,30 @@
 <template>
-  <XyzTransition appear duration="auto">
-    <div class="page-wrap">
-      <div class="page-hero" xyz="fade small stagger ease-out-back">
-        <v-avatar class="xyz-nested">
-          <img src="https://avatars.githubusercontent.com/u/56793204?s=96&v=4" alt="Urturn">
-        </v-avatar>
-        <p class="hero-text xyz-nested">Hi! I'm urturn. Student of Titech.</p>
-      </div>
-      <div class="page-features" xyz="fade flip-down stagger duration-10 delay-2 ease-out-back">
-        <Card class="xyz-nested" url="https://20jun01.github.io/topublish/" />
-      </div>
-      <div class="page-section" xyz="fade flip-down stagger duration-10 delay-2 ease-out-back">
-        <Card class="xyz-nested" url="https://mushikui.trasta.dev/" />
-      </div>
-      <div class="page-footer" xyz="fade bottom ease-in-out delay-10">
-        <div class="footer-logo xyz-nested" xyz="fade left ease-in-out delay-10"></div>
-        <div class="footer-right" xyz="fade up stagger ease-in-out delay-10">
-          <Card class="xyz-nested" url="" />
-        </div>
-      </div>
-    </div>
-  </XyzTransition>
+  <v-container>
+    <XyzTransition appear duration="auto">
+      <v-row no-gutters>
+        <v-col cols="12" sm="4" class="page-wrap">
+          <div class="page-hero" xyz="fade small stagger ease-out-back">
+            <v-avatar class="xyz-nested">
+              <img src="https://avatars.githubusercontent.com/u/56793204?s=96&v=4" alt="Urturn">
+            </v-avatar>
+            <p class="hero-text xyz-nested">Hi! I'm urturn. Student of Titech.</p>
+          </div>
+          <div class="page-features" xyz="fade flip-down stagger duration-10 delay-2 ease-out-back">
+            <Card class="xyz-nested" url="https://20jun01.github.io/topublish/" />
+          </div>
+          <div class="page-section" xyz="fade flip-down stagger duration-10 delay-2 ease-out-back">
+            <Card class="xyz-nested" url="https://mushikui.trasta.dev/" />
+          </div>
+          <div class="page-footer" xyz="fade bottom ease-in-out delay-10">
+            <div class="footer-logo xyz-nested" xyz="fade left ease-in-out delay-10"></div>
+            <div class="footer-right" xyz="fade up stagger ease-in-out delay-10">
+              <Card class="xyz-nested" url="https://sample-front-weld.vercel.app/" />
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </XyzTransition>
+  </v-container>
 </template>
 <script>
 import Card from '@/components/CardWithOGP.vue'
