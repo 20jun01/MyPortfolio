02e6bbec77699656dbf80603b11275e45e681af9
20jun01 <10junjun20@gmail.com>
2023-02-06 15:44:32 +0900

    ➕ add animxyz

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-
+import VueAnimXYZ from '@animxyz/vue'
+import '@animxyz/core' // Import css here if you haven't elsewhere
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnimXYZ)
 
 new Vue({
   vuetify,
