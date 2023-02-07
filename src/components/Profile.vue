bf2335c7eaa39964109d3289de43cbcddba935bd
20jun01 <10junjun20@gmail.com>
2023-02-07 11:40:06 +0900

    ⚡️ change greet to divided

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,15 +2,23 @@
   <v-container>
     <XyzTransition appear duration="auto">
       <div>
-        <div class="page-hero" xyz="fade small stagger ease-out-back">
-          <v-avatar class="xyz-nested">
-            <img src="https://avatars.githubusercontent.com/u/56793204?s=96&v=4" alt="Urturn">
-          </v-avatar>
-          <p class="hero-text xyz-nested">Hi! I'm urturn.</p>
-          <p class="hero-text xyz-nested">I'm a Student of Titech.</p>
-          <p class="hero-text xyz-nested">I contributed to the following projects as a programmer.</p>
-        </div>
-        <v-row no-gutters>
+        <v-row class="page-hero" xyz="fade small stagger ease-out-back">
+          <v-col cols="12">
+            <v-avatar class="xyz-nested">
+              <img src="https://avatars.githubusercontent.com/u/56793204?s=96&v=4" alt="Urturn">
+            </v-avatar>
+          </v-col>
+        </v-row>
+        <v-row class="page-hero" xyz="fade small stagger ease-out-back">
+          <v-card class="d-inline-flex justify-center mb-6 greet xyz-nested"
+            flat tile>
+            <div class="hero-text">
+              Hi!I'm&nbsp; 
+            </div>
+            <div class="hero-text greet-name">urturn!</div>
+          </v-card>
+          <v-col cols="12" class="hero-text xyz-nested">I'm a Student of Titech.</v-col>
+          <v-col cols="12" class="hero-text xyz-nested">I contributed to the following projects as a programmer.</v-col>
           <v-col cols="12" xl="2" lg="4" md="4" class="page-wrap">
             <div class="xyz-nested" xyz="fade flip-down stagger duration-10 delay-2 ease-out-back">
               <Card url="https://20jun01.github.io/topublish/" />
@@ -41,7 +49,11 @@ export default {
 }
 </script>
 <style scoped>
-.hero-text{
+.hero-text {
   color: #B1D4E0;
+}
+
+.greet{
+  background-color: transparent;
 }
 </style>
