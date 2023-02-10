7fff0e56083e96eb0ac47861babf093c3bc6a0ba
20jun01 <10junjun20@gmail.com>
2023-02-10 22:40:15 +0900

    ⚡️ responsive header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,8 @@
 <template>
-    
     <v-app-bar dense flat color="transparent" app>
         <v-spacer />
         <v-row class="justify-end">
-            <v-col cols="1" v-for="link in links" :key="link.name">
+            <v-col cols="2" xs="2" sm="2" md="2" lg="1" xl="1" v-for="link in links" :key="link.name">
                 <router-link class="router_text" :to=link.href>{{ link.name }}</router-link>
             </v-col>
             <v-col cols="1"></v-col>
